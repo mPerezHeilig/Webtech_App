7cc45977dd96b511f26b42c59f5b1521b26027e1
Marcia Perez Heilig <perez-he@hm.edu>
2025-01-16 09:31:41 +0100

    move establishing database connection to server.ts

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,12 +2,23 @@
 
 import dotenv from 'dotenv';
 import app from './app';
+import mongoose from 'mongoose';
 
 dotenv.config(); // load environment variables
 
 const port = process.env.PORT; // set port from environment variables
+const mongo_uri = process.env.MONGO_URI; // set connection string from environment variables
 
 // start server
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
 });
+
+// establish database connection
+const connectDatabase = () => {
+    mongoose.connect(mongo_uri || "").then(() => {
+        console.log('Connected to database');
+    }).catch((error) => {
+        console.log('Database connection failed', error);
+    });
+};
